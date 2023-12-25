b94a22e2bb853bca8e4d9c0a2281eb40300a1ec0
natanfeitosa <natansantosapps@gmail.com>
2023-12-25 10:42:38 -0300

    fix: Corrige a lógica de adição do lexer e limpeza de cache

--- register_portuscript_lexer.py
+++ register_portuscript_lexer.py
@@ -1,33 +1,48 @@
-import shutil, sys, pygments, re, os
+import shutil, sys, pygments, re
 
 from pathlib import Path
 from importlib import reload
+from pygments.lexers import get_lexer_by_name
+from pygments.util import ClassNotFound
 
-pygments_path = sys.modules['pygments'].__path__[0]
-pygments_path_lexers = Path(pygments_path) / 'lexers'
-pygments_map_lexers = pygments_path_lexers / '_mapping.py'
+pygments_path = sys.modules["pygments"].__path__[0]
+pygments_path_lexers = Path(pygments_path) / "lexers"
+pygments_map_lexers = pygments_path_lexers / "_mapping.py"
 
-portuscript_original_file_lexer = Path().absolute() / 'lexers' / 'portuscript_lexer.py'
-portuscript_in_pygments = pygments_path_lexers / 'portuscript.py'
+portuscript_original_file_lexer = (
+    Path(__file__).absolute().parent / "lexers" / "portuscript_lexer.py"
+)
+portuscript_in_pygments = pygments_path_lexers / "portuscript.py"
 
-def copy_portuscript_lexer(create: bool):
-    shutil.copyfile(
-        portuscript_original_file_lexer,
-        portuscript_in_pygments
-    )
-    
-    if create:
+
+def copy_portuscript_lexer():
+    shutil.copyfile(portuscript_original_file_lexer, portuscript_in_pygments)
+
+    try:
+        get_lexer_by_name("ptst")
+    except ClassNotFound:
         content = pygments_map_lexers.read_text()
         pygments_map_lexers.write_text(
-            re.sub(r'(\})$', "'portuscript': ('pygments.lexers.portuscript', 'Portuscript', ('ptst', 'portuscript'), ('*.ptst',), ()),}", content)
+            re.sub(
+                r"(\})$",
+                "'portuscript': ('pygments.lexers.portuscript', 'Portuscript', ('ptst', 'portuscript'), ('*.ptst',), ()),}",
+                content,
+            )
         )
-    
-    for cache in (pygments_path_lexers / '..').glob('./**/__pycache__/**/*'):
-        os.remove(str(cache.absolute()))
+    except Exception as e:
+        print("[erro]: %s" % e)
+
+    for cache in (pygments_path_lexers / "..").glob("./**/__pycache__/"):
+        shutil.rmtree(cache.absolute())
 
     reload(pygments)
 
+
 create = not portuscript_in_pygments.exists()
-update = not create and portuscript_original_file_lexer.stat().st_mtime > portuscript_in_pygments.stat().st_mtime
+update = (
+    not create
+    and portuscript_original_file_lexer.stat().st_mtime
+    > portuscript_in_pygments.stat().st_mtime
+)
 if create or update:
-    copy_portuscript_lexer(create)
+    copy_portuscript_lexer()
